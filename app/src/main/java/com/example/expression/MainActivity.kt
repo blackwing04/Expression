dd352d9639c6f3fec88967aa118fc040b4e9b52c
Blackwing <hh4654075hh@gmail.com>
2022-10-12 18:23:15 +0800

    完成加法

--- app/src/main/java/com/example/expression/MainActivity.kt
+++ app/src/main/java/com/example/expression/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.expression
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
+lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var btCal : Button
+    lateinit var tvResult : TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etA = findViewById(R.id.etA)
+        etB = findViewById(R.id.etB)
+        btCal = findViewById(R.id.btCal)
+        tvResult = findViewById(R.id.tvResult)
+        btCal.setOnClickListener {
+            val a : Double = (etA.text.toString().toDouble()) + (etB.text.toString().toDouble())
+            tvResult.text = a.toString()
+        }
     }
+
+
 }
